7437be58ea9b6ee8a4ebfc3ae389f94409b2222c
Sam Vervaeck <samvv@pm.me>
2026-03-13 10:05:20 +0100

    Also export some functions from validator.ts in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@
 export { assertNever, applyMixins } from "./util.js"
 
 export * from "./common.js"
+export {
+  lazyValidate,
+  validate,
+  isValid,
+  registerValidator,
+  ValidationError,
+  RecurseFn,
+  Validators,
+  ValidateFn,
+  ValidateOptions
+} from "./validators.js"
 
 import * as types from "./types/index.js"
 
