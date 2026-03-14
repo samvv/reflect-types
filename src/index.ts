fbe5c3a8fc42057a83e3fbd2b156332d5b48c7dc
Sam Vervaeck <samvv@pm.me>
2026-03-14 14:55:34 +0100

    Ensure only type constructor functions are namespaced

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,32 @@ export {
   ValidateOptions
 } from "./validators.js"
 
-import * as types from "./types/index.js"
+export type {
+  ArrayType,
+  BooleanType,
+  CallableType,
+  DateType,
+  Encoding,
+  LiteralType,
+  LiteralValue,
+  NullType,
+  NumberCategory,
+  NumberType,
+  ObjectType,
+  OptionalType,
+  PromiseType,
+  StringType,
+  TupleType,
+  UUID,
+  UUID4,
+  UUID4Type,
+  UUID7,
+  UUID7Type,
+  UndefinedType,
+  UnionType,
+  UnknownType,
+} from "./types/index.js"
+
+import * as types from "./namespaced.js";
 
 export { types };
